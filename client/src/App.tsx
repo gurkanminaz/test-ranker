2cf51795c4cf8afe299cd15d657cd92ec1a4b2d3
Gürkan Minaz <gurkanminaz@gmail.com>
2023-07-05 17:41:29 +0300

    Reconnecting React Client to NestJS Via Socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { devtools } from 'valtio/utils';
 
 import './index.css';
 import Pages from './Pages';
-import { state } from './state';
+import { actions, state } from './state';
 import { useSnapshot } from 'valtio';
 import Loader from './components/ui/Loader';
+import { getTokenPayload } from './util';
 
 devtools(state, 'app state');
 const App: React.FC = () => {
   const currentState = useSnapshot(state);
+
+  useEffect(() => {
+    console.log('App useEffect - check token and send to proper page');
+
+    actions.startLoading();
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      actions.stopLoading();
+      return;
+    }
+
+    const { exp: tokenExp } = getTokenPayload(accessToken);
+    const currentTimeInSeconds = Date.now() / 1000;
+
+    // Remove old token
+    if (tokenExp < currentTimeInSeconds - 10) {
+      localStorage.removeItem('accessToken');
+      actions.stopLoading();
+      return;
+    }
+
+    // reconnect to poll
+    actions.setPollAccessToken(accessToken); // needed for socket.io connection
+    // socket initialization on server sends updated poll to the client
+    actions.initializeSocket();
+  }, []);
+
   return (
     <>
       <Loader isLoading={currentState.isLoading} color="orange" width={120} />
--- client/src/socket-io.ts
+++ client/src/socket-io.ts
@@ -28,6 +28,12 @@ export const createSocketWithHandlers = ({
     console.log(
       `Connected with socket ID: ${socket.id}, UserID: ${state.me?.id} will join room ${state.poll?.id}`
     );
+    actions.startLoading();
+  });
+
+  socket.on('connect_error', () => {
+    console.log('Failed to connect socket');
+    actions.stopLoading();
   });
 
   socket.on('poll_updated', (poll) => {
